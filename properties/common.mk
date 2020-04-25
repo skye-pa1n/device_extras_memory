65b39d7f2434f3fdedc4a07fefc901eea1f1fca4
David Sehr <sehr@google.com>
2020-04-25 15:50:40 -0700

    extra-memory: Use 64bit dex2oat on all cores
    Bug: 153380900
    Test: boot and install an application
    Change-Id: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095

--- properties/common.mk
+++ properties/common.mk
@@ -1,4 +1,6 @@
 # ART
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    dalvik.vm.dex2oat64.enabled=true \
     dalvik.vm.dex2oat-filter=quicken \
-    dalvik.vm.image-dex2oat-filter=quicken
+    dalvik.vm.image-dex2oat-filter=quicken \
+    ro.sys.fw.dex2oat_thread_count=8
