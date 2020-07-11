5a48df44ce4b08b212156f8126053d0c752ab150
Hazari PraveenKumar <Hazari PraveenKumar@codeaurora.org>
2020-07-11 13:53:25 +0530

    extra-memory: Disable VSync for CPU rendered apps
    Change-Id: I2ad979f7f5cdbf52131e43e7083e59be92a4bef9

--- properties/common.mk
+++ properties/common.mk
@@ -4,3 +4,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.dex2oat-filter=quicken \
     dalvik.vm.image-dex2oat-filter=quicken \
     ro.sys.fw.dex2oat_thread_count=8
+
+# Render
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.cpurend.vsync=false
