5ef926b3f18d3cf6980bf6ee0baa28ad5d4bdc0c
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    extra-product: Speed profile services to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- product.mk
+++ product.mk
@@ -9,3 +9,6 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
