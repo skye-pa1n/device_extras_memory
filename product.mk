26b3b9add770e3986aace5c0397b74a17d0aa016
Harshit Jain <harshitjain6751@gmail.com>
2021-08-22 16:36:13 +0800

    extra-product: Strip off some debug packages
    * Ref: https://source.android.com/devices/tech/dalvik/configure#compilation_options
    
    Change-Id: Iad073ec85f5cfe5be5a8d1944dd7cff521077dbe

--- product.mk
+++ product.mk
@@ -13,3 +13,5 @@ PRODUCT_COPY_FILES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+USE_DEX2OAT_DEBUG := false
