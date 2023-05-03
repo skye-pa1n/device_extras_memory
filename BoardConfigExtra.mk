2d4334ff3a2f54db942fa23da8b2904876e92cee
clarencelol <clarencekuiek@icloud.com>
2023-05-03 11:51:03 +0800

    extra-memory: Fully switched to jemalloc
    * https://review.arrowos.net/c/ArrowOS/android_build/+/18089
    
    Change-Id: I7576a69c2e181b5fa39cda70393b12828bd304b5
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -6,6 +6,12 @@
 
 EXTRA_PATH := device/extras/memory
 
+ifeq ($(TARGET_CONFIG_JEMALLOC),true)
+# Jemalloc
+MALLOC_SVELTE := true
+MALLOC_SVELTE_FOR_LIBC32 := true
+endif
+
 # Properties
 ifeq ($(TARGET_CONFIG_GFX),true)
 TARGET_SYSTEM_EXT_PROP += $(EXTRA_PATH)/properties/system_ext.prop
