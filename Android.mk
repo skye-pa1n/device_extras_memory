83640b6d0d33a0a9a91d8a266f48e6c1a68564b8
Vhmit <viktor.9630@protonmail.com>
2024-11-09 18:01:23 -0300

    extras-memory: Initial commit from repository
    Change-Id: Ie5e0be7271c183eb2f852c30e30bb3df1cdeed60
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

---
+++ Android.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
---
+++ README.md
@@ -0,0 +1,7 @@
+### Build the service
+
+Add inherit on your device.mk:
+
+```makefile
+include device/extras/memory/product.mk
+```
