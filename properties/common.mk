61370585984473f88972cc1a31af15337cda3797
Vhmit <viktor.9630@protonmail.com>
2024-11-10 01:01:41 -0300

    extra-memory: Increase RAM managent
    * The ro.config.fha_enable parameter on Android refers to a system setting
    that is related to the Fast Home Animation functionality on Android devices.
    When enabled, it can influence how the user interface behaves when transitioning
    between the home screen and apps, improving fluidity and responsiveness when
    opening and closing apps.
    
    Change-Id: Iebef9133f6cf446183967ed9b345b87ee69bdec6
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- properties/common.mk
+++ properties/common.mk
@@ -5,6 +5,20 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.image-dex2oat-filter=quicken \
     ro.sys.fw.dex2oat_thread_count=8
 
+ifeq ($(CONFIG_FHA_APPS),true)
+# RAM managent
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
+endif
+
 # Render
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.cpurend.vsync=false
