9f08d821a68cbb3cd6a2699c0c264caaadafdb3e
ImSpiDy <SpiDy2713@gmail.com>
2024-11-10 00:40:59 -0300

    extra-memory: Set dex2oat filter to quicken
    * Run DEX code verification and optimize some DEX
      instructions to get better interpreter performance.
    
    Change-Id: I46ee28dcbf4e00ec8b6400ecae1ffbec9214846e
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- product.mk
+++ product.mk
@@ -10,6 +10,9 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+# Properties
+include $(LOCAL_PATH)/properties/common.mk
+
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
---
+++ properties/common.mk
@@ -0,0 +1,4 @@
+# ART
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    dalvik.vm.dex2oat-filter=quicken \
+    dalvik.vm.image-dex2oat-filter=quicken
